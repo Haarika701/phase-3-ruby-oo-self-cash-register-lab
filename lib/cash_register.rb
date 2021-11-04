088bc6a01248b746b00e5699d6b0a6f24ee03456
HaarikaR <hramadugu@gmail.com>
2021-11-03 17:23:03 -0700

    commit tree 34d3a3916200aa6eff845dc5e7ad30e80beb74d0

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,52 @@
 
+require 'pry'
+class CashRegister
+
+    # @@all = []
+  attr_accessor :total,:discount
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @item = []
+    end
+
+
+    def total
+       @total 
+    end
+
+    def discount
+        @discount
+    end
+
+    def add_item (title,price,quantity=1)
+    
+    @total = total + price * quantity
+    quantity.items do
+        items<< title
+    end
+
+    def quantity
+        @quantity
+    end
+
+    def price
+        @price
+    end
+
+    def apply_discount
+        if self.discount != 0
+            discount_as_percent = (100.0 - self.discount.to_f) / 100
+            self.total = (self.total * discount_as_percent).to_i
+            "After the discount, the total comes to $#{self.total}."
+          else
+            "There is no discount to apply."
+          end
+        end
+
+    \
+
+end
+
+# puts haarika = CashRegister.new(100,0.20)
+# binding.pry
